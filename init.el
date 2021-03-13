9d2ba00927078e68717e758b0b082dfcee7593d3
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-03-13 18:03:40 -0500

    Update selected packages

--- init.el
+++ init.el
@@ -46,3 +46,10 @@
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
       )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-noter yaml-mode xterm-color which-key use-package treemacs-projectile speed-type solarized-theme protobuf-mode pdf-tools magit lsp-pyright helm-xref form-feed dockerfile-mode dired-du dap-mode counsel bazel-mode)))
