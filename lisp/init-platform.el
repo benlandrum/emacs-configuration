c9f0fc5bdc6a42e41f612f354a0e8521eb5d9292
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-03-27 10:38:28 -0400

    Use Mac texbin path

--- lisp/init-platform.el
+++ lisp/init-platform.el
@@ -1,6 +1,11 @@
 (when (string= system-type "darwin")
   (setq dired-use-ls-dired nil
-	ns-alternate-modifier '(:ordinary super :function super :mouse super)
-	ns-command-modifier 'meta))
+	ns-alternate-modifier '(:ordinary super
+				:function super
+				:mouse super)
+	ns-command-modifier 'meta)
+  (let ((texbin-path "/Library/TeX/texbin"))
+    (setenv "PATH" (concat (getenv "PATH") ":" texbin-path))
+    (setq exec-path (append exec-path (list texbin-path)))))
 
 (provide 'init-platform)
