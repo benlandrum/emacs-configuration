a2562b63ffb0ab932dd350b2555a5a703751f47a
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-04-05 12:16:51 -0400

    init-org.el: Fix indentation

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,7 +1,7 @@
 ;;; init-org.el --- Customize org and related modes.
 
 ;;; Commentary:
-;; Only a few settings currently.
+;; The biggest customizations are indented startup and org-noter bindings.
 
 ;;; Code:
 
@@ -9,9 +9,18 @@
   :config
   (setq org-startup-indented t
 	org-agenda-files '(my-org-agenda-files))
-  :hook (visual-line-mode))
 
-(use-package org-noter)
+  ;; Sync LaTeX fragment scaling with text scaling.
+  ;; https://emacs.stackexchange.com/questions/3387/how-to-enlarge-latex-fragments-in-org-mode-at-the-same-time-as-the-buffer-text
+  (defun update-org-latex-fragments ()
+    (org-latex-preview '(64))
+    (plist-put org-format-latex-options :scale text-scale-mode-amount)
+    (org-latex-preview '(16)))
+  (add-hook 'text-scale-mode-hook 'update-org-latex-fragments)
+  (add-hook 'org-mode-hook #'visual-line-mode))
+
+(use-package org-noter
+  :bind ("M-i" . org-noter-insert-precise-note))
 
 (provide 'init-org)
 
