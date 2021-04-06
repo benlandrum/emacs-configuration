ffbccfca39bb3d8525d94b74280733d28d7114e7
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-04-06 18:15:23 -0400

    Set org agenda files recursively, following symbolic links

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  my-epdfinfo-program "/Users/blandrum/.local/bin/epdfinfo"
  my-lsp-clients-clangd-executable "/ssh:vagrant@10.211.55.4#22:/usr/bin/clangd"
  my-lsp-debug t
- my-org-agenda-files (list "~/org/")
+ my-org-directory "~/org"
  my-minimum-emacs-version "27.1"
  my-vagrant-vm-address "10.211.55.4#22"
  my-vagrant-vm-path "/Users/blandrum/beeswax/vms/serving"
