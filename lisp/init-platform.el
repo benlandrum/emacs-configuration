1a0ab486750fdc59c88071d049edfd055cb97c3b
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-05-12 21:13:57 -0400

    Use a common path setter

--- lisp/init-platform.el
+++ lisp/init-platform.el
@@ -5,9 +5,11 @@
 
 ;;; Code:
 
-(let ((usr-local-bin-path "/usr/local/bin"))
-  (setenv "PATH" (concat (getenv "PATH") ":" usr-local-bin-path))
-  (setq exec-path (append exec-path (list usr-local-bin-path))))
+(defun my-add-to-env-path (p)
+  (setenv "PATH" (concat (getenv "PATH") ":" p))
+  (push p exec-path))
+
+(my-add-to-env-path "/usr/local/bin")
 
 (when (string= system-type "darwin")
   (setq dired-use-ls-dired nil
@@ -15,9 +17,7 @@
 				:function super
 				:mouse super)
 	ns-command-modifier 'meta)
-  (let ((texbin-path "/Library/TeX/texbin"))
-    (setenv "PATH" (concat (getenv "PATH") ":" texbin-path))
-    (setq exec-path (append exec-path (list texbin-path)))))
+  (my-add-to-env-path "/Library/TeX/texbin"))
 
 (provide 'init-platform)
 
