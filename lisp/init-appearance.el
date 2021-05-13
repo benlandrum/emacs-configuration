f347a4014cb7107bb2eae6881e407da9cf15076d
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-05-12 21:13:38 -0400

    Remove bars

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -8,6 +8,11 @@
 (use-package solarized-theme)
 (load-theme 'solarized-light t)
 
+;; Hide the menu bar, tool bar, and scroll bars.
+(when (fboundp 'menu-bar-mode)   (menu-bar-mode   -1))
+(when (fboundp 'tool-bar-mode)   (tool-bar-mode   -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 (provide 'init-appearance)
 
 ;;; init-appearance.el ends here
