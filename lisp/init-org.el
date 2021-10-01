6eedf1c6b4748a0e136cdfcb97e6355e70160b95
Benjamin Joseph Landrum <ben.landrum@gmail.com>
2021-10-01 17:26:35 -0400

    Try some org-id functions

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -29,7 +29,37 @@
 			 text-scale-mode-amount))
     (org-latex-preview '(16)))
   (add-hook 'text-scale-mode-hook 'update-org-latex-fragments)
-  (add-hook 'org-mode-hook #'visual-line-mode))
+  (add-hook 'org-mode-hook #'visual-line-mode)
+
+  ;; https://writequit.org/articles/emacs-org-mode-generate-ids.html
+  (defun my-org-custom-id-get (&optional pom create prefix)
+    "Get the CUSTOM_ID property of the entry at point-or-marker POM.
+     If POM is nil, refer to the entry at point. If the entry does
+     not have an CUSTOM_ID, the function returns nil. However, when
+     CREATE is non nil, create a CUSTOM_ID if none is present
+     already. PREFIX will be passed through to `org-id-new'. In any
+     case, the CUSTOM_ID of the entry is returned."
+    (interactive)
+    (org-with-point-at pom
+      (let ((id (org-entry-get nil "CUSTOM_ID")))
+	(cond
+	 ((and id (stringp id) (string-match "\\S-" id))
+          id)
+	 (create
+          (setq id (org-id-new (concat prefix "h")))
+          (org-entry-put pom "CUSTOM_ID" id)
+          (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+          id)))))
+
+  (defun my-org-add-ids-to-headlines-in-file ()
+    "Add CUSTOM_ID properties to all headlines in the
+     current file which do not already have one."
+    (interactive)
+    (org-map-entries (lambda () (my-org-custom-id-get (point) 'create))))
+
+  ;; Automatically add ids to captured headlines.
+  (add-hook 'org-capture-prepare-finalize-hook
+            (lambda () (my-org-custom-id-get (point) 'create))))
 
 ;; Not technically org, but I always use this through org.
 ;; "C-c C-e h o" renders to my browser with syntax highlighting with this.
