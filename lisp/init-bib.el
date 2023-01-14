0ae7fa035258a0816dda0c4b16f5f57de901967a
Ben Landrum <ben.landrum@gmail.com>
2023-01-14 12:05:32 -0500

    Add TODO items for autokey

--- lisp/init-bib.el
+++ lisp/init-bib.el
@@ -7,6 +7,10 @@
 
 (use-package biblio :ensure t)
 
+;; TODO: Fix automatic entries with name conflicts (happens with year-only keys).
+;; TODO: Always use the title for the autokeys.
+;;       See http://www.jonathanleroux.org/bibtex-mode.html.
+
 (use-package ebib
   :ensure t
   :bind (("C-c e" . ebib)
