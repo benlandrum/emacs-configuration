73b45cc2c0f593cc462f8c3d61a8d1e69778def1
Ben Landrum <ben.landrum@gmail.com>
2023-04-17 07:28:59 -0400

    Add Asymptote

--- lisp/init-filetypes.el
+++ lisp/init-filetypes.el
@@ -33,6 +33,20 @@
   (setq indent-tabs-mode nil))
 (add-hook 'js-mode-hook 'my-js-mode-hook)
 
+;; ob-asymptote, org-babel, and asymptote.
+;; ob-asymptote will migrate to org.
+;; https://github.com/emacsmirror/org-contrib/commit/fff6c888065588527b1c1d7dd7e41c29ef767e17
+(use-package org-contrib)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((asymptote . t)))
+
+(add-to-list 'load-path my-asymptote-dir)
+(autoload 'asy-mode "asy-mode.el" "Asymptote major mode." t)
+(autoload 'lasy-mode "asy-mode.el" "hybrid Asymptote/Latex major mode." t)
+(autoload 'asy-insinuate-latex "asy-mode.el" "Asymptote insinuate LaTeX." t)
+(add-to-list 'auto-mode-alist '("\\.asy$" . asy-mode))
+
 (provide 'init-filetypes)
 
 ;;; init-filetypes.el ends here
